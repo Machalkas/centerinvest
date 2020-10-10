42f441870dd9406f37d9431015af5ca72dd6e313
Machalka <al1999dk@gmail.com>
2020-10-10 19:28:37 +0300

    сделал вывод формы (без стилей}

--- main/views.py
+++ main/views.py
@@ -9,10 +9,12 @@ def index(request):
         form=QueueForm(request.POST)
         if form.is_valid():
             q=Queue.objects.all().order_by('id')
-            last_num=None
+            last_num=addres=time=None
             for i in range(len(q)-1,-1,-1):
                 if q[i].is_active==True:
                     last_num=q[i].number
+                    addres=q[i].branch
+                    time=q[i].time
                     break
             if last_num==None:
                 q_num="I0"
@@ -22,7 +24,7 @@ def index(request):
             new_cl=form.save()
             new_cl.number=q_num
             new_cl.save()
-            return HttpResponse(q_num)
+            return render(request,"main/ticket.html",{'num':q_num, 'time':time, 'addres':addres})# HttpResponse(q_num)
     else:
         form=QueueForm()
         form.fields['number'].widget = forms.HiddenInput()
